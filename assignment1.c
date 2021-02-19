c4ba0ca0bbfdbab00cafe6d8cdc736b55f009aab
RahulMishra <rahuljyotimishra2002@gmail.com>
2021-02-19 19:31:12 +0530

    commit it

---
+++ assignment1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+	printf("Name:- Rahul Jyoti Mishra\nreg no.:-20IT015\nBranch:-Information Technology\n");
+	printf("Name:- Rushali Mishra\nreg no.:-20IT045\nBranch:-Information Technology\n");
+	
+	return 0;
+}
